fd0b0c9b1c7c0da9a28f961020785948bcd97f6f
kubealex <al.rossi87@gmail.com>
2021-12-08 16:19:51 +0100

    Progress on day 3

---
+++ day3/day3-part1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("./aoc-d3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	var result [12]int64
+	var revResult [12]int64
+	var linecount (int64)
+	resultString := ""
+	revResultString := ""
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		linecount++
+		line := scanner.Text()
+		for i := 0; i < 12; i++ {
+			if line[i] == 48 {
+				result[i]++
+			}
+		}
+	}
+	for i, element := range result {
+
+		if element-linecount/2 > 0 {
+			result[i] = 1
+			revResult[i] = 0
+		} else {
+			result[i] = 0
+			revResult[i] = 1
+		}
+		revResultString = revResultString + strconv.FormatInt(revResult[i], 2)
+		resultString = resultString + strconv.FormatInt(result[i], 2)
+	}
+	gamma, _ := strconv.ParseUint(resultString, 2, 16)
+	epsilon, _ := strconv.ParseUint(revResultString, 2, 16)
+	fmt.Printf("Result: \n Gamma: %d\n Epsilon: %d \n Result: %d\n ", gamma, epsilon, gamma*epsilon)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
