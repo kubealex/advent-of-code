f3d19ef6e4736a0db4893020f072ae69e8e1c7e2
kubealex <al.rossi87@gmail.com>
2021-12-12 19:50:13 +0100

    Started day4

--- day4/day4-part1.go
+++ day4/day4-part1.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
+type BingoBoard struct {
+}
+
+func Split(r rune) bool {
+	return r == ',' || r == ' '
+}
+
 func main() {
-	file, err := os.Open("./aoc-d3")
+	file, err := os.Open("./aoc-d4")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
+	var test []string
 	for scanner.Scan() {
-		line := scanner.Text()s
+		line := scanner.Text()
+		test = strings.FieldsFunc(line, Split)
 	}
+	fmt.Println(test)
 }
 
 //TODO
